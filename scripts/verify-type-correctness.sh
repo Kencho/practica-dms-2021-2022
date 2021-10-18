561aa6f6fd4dc8efaf15a2cfce8ee540d25ae643
Jesus Alonso Abad <jesusaa@ubu.es>
2021-10-19 00:17:55 +0200

    Bugfix: Type correctness verification (CI)

--- scripts/verify-type-correctness.sh
+++ scripts/verify-type-correctness.sh
@@ -3,16 +3,16 @@
 SUCCESS=0
 
 echo "[dms2122common]"
-mypy components/dms2122common/dms2122common
+mypy --install-types --non-interactive components/dms2122common/dms2122common
 SUCCESS=$((${SUCCESS}+$?))
 echo "[dms2122auth]"
-mypy components/dms2122common/dms2122common components/dms2122auth/dms2122auth
+mypy --install-types --non-interactive components/dms2122common/dms2122common components/dms2122auth/dms2122auth
 SUCCESS=$((${SUCCESS}+$?))
 echo "[dms2122backend]"
-mypy components/dms2122common/dms2122common components/dms2122backend/dms2122backend
+mypy --install-types --non-interactive components/dms2122common/dms2122common components/dms2122backend/dms2122backend
 SUCCESS=$((${SUCCESS}+$?))
 echo "[dms2122frontend]"
-mypy components/dms2122common/dms2122common components/dms2122frontend/dms2122frontend
+mypy --install-types --non-interactive components/dms2122common/dms2122common components/dms2122frontend/dms2122frontend
 SUCCESS=$((${SUCCESS}+$?))
 
 exit ${SUCCESS}
